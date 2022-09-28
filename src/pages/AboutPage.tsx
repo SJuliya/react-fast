45638a8b262c9eb0f35408aca00652ce38b144b5
Juliya Sartasova <anjee2007@mail.ru>
2022-09-28 16:22:48 +0300

    finish

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function AboutPage() {
+    return (
+        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium alias at atque debitis delectus doloribus eum expedita explicabo fugit id ipsa magnam maxime minus molestiae non nulla perferendis porro provident quaerat quas quisquam recusandae repellendus sed sunt tempora tempore ut vel veniam veritatis vero, vitae voluptatem voluptatum. Fugit, voluptates?</div>
+    )
+}
